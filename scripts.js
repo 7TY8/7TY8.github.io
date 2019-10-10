4dbaf8090219c20674ab67bb68c3f0c157229987
7TY8 <53916932+7TY8@users.noreply.github.com>
2019-10-10 23:20:51 +0600

    initial commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+	$('html, body').animate({
+		scrollTop: 0
+	});
+})
+
+$('.btn-main').click(function () {
+	$('html, body').animate({
+		scrollTop: 654
+	}, 1000);
+})
+
+$('.fixed').click(function () {
+	if ($('html, body').scrollTop() <= 5886) {
+		$('html, body').animate({
+			scrollTop: $('html, body').scrollTop() + 654
+		}, 1000);
+	}
+
+	else if ($('html, body').scrollTop() >= 5886) {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 2000);
+	}
+})
+
+setInterval(function () {
+	if ($('html, body').scrollTop() >= 327) {
+		$('.fixed').css('opacity', '1')
+	}
+
+	if ($('html, body').scrollTop() <= 327) {
+		$('.fixed').css('opacity', '0')
+	}
+})
